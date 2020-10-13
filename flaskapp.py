fe572064cbe70ac737dcfb627411789448175655
Taeyoon Kim <tykimseoul@gmail.com>
2020-10-13 15:13:53 +0900

    update: support cvf instead of arxiv

--- flaskapp.py
+++ flaskapp.py
@@ -11,7 +11,6 @@ from io import BytesIO
 import os
 
 app = Flask(__name__)
-base_url = 'https://arxiv.org/abs/{}'
 thumbnails_dir = Path("/tmp/thumbnails")
 thumbnails_dir.mkdir(parents=True, exist_ok=True)
 
@@ -26,19 +25,14 @@ def get_html(url, t):
         return get_html(url, t + 5)
 
 
-def parse_page(code):
-    html = get_html(base_url.format(code), 5)
+def parse_cvf_page(url):
+    html = get_html(url, 5)
     document = BeautifulSoup(html.text, "html.parser")
-    title = document.select_one('h1.title').text
-    title = re.sub(r'^Title:', '', title)
-    authors = list(map(lambda a: a.text, document.select('div.authors > a')))
-    abstract = document.select_one('blockquote.abstract').findAll(text=True, recursive=False)
-    abstract = list(filter(lambda a: a != '\n', abstract))
-    abstract = ' '.join(abstract)
-    abstract = re.sub(r'[\n\r]', ' ', abstract)
-    abstract = abstract.strip()
-    abstract = re.sub(r'\s+', ' ', abstract)
-    thumbnail = extract_thumbnail(code)
+    title = document.select_one('div#papertitle').text
+    authors = document.select_one('div#authors > b > i').text
+    authors = authors.split(', ')
+    abstract = document.select_one('div#abstract').text
+    thumbnail = extract_cvf_thumbnail(url)
     buffer = BytesIO()
     thumbnail.save(buffer, format="JPEG")
     thumbnail = base64.b64encode(buffer.getvalue()).decode('utf-8')
@@ -49,39 +43,43 @@ def parse_page(code):
     return {'title': title, 'authors': authors, 'abstract': abstract, 'thumbnail': thumbnail}
 
 
+def extract_cvf_thumbnail(url):
+    code = re.match(r'https://openaccess.thecvf.com/(\w+)/html/([\w-]+).html', url).groups()
+    if not check_thumbnail(code[1]):
+        response = requests.get('https://openaccess.thecvf.com/{}/papers/{}.pdf'.format(code[0], code[1]))
+        store_thumbnail(response, code[1])
+    return read_thumbnail(code[1])
+
+
 @app.route('/', methods=['GET'])
 def index():
     return 'hello world'
 
 
-@app.route('/arxiv', methods=['GET'])
+@app.route('/paper', methods=['GET'])
 def get_paper_data():
-    code = request.args.get('code')
-    data = parse_page(code)
+    link = request.args.get('link')
+    if re.match(r'https://openaccess.thecvf.com/(\w+)/html/([\w-]+).html', link):
+        data = parse_cvf_page(link)
+    else:
+        data = {}
     return json.dumps(data)
 
 
-def extract_thumbnail(code):
-    if not check_thumbnail(code):
-        response = requests.get("https://arxiv.org/pdf/{}.pdf".format(code))
-        with open('/tmp/downloaded.pdf', 'wb') as f:
-            f.write(response.content)
-            doc = fitz.open('/tmp/downloaded.pdf')
-            for i in range(len(doc)):
-                images = doc.getPageImageList(i)
-                if len(images) > 0:
-                    img = images[0]
-                    print(img)
-                    xref = img[0]
-                    pix = fitz.Pixmap(doc, xref)
-                    pix = fitz.Pixmap(pix, 0)
-                    store_thumbnail(pix, code)
-                    break
-    return read_thumbnail(code)
-
-
-def store_thumbnail(pix, key):
-    pix.writeImage('{}/{}.jpg'.format(str(thumbnails_dir), key))
+def store_thumbnail(response, key):
+    with open('downloaded.pdf', 'wb') as f:
+        f.write(response.content)
+        doc = fitz.open('downloaded.pdf')
+        for i in range(len(doc)):
+            images = doc.getPageImageList(i)
+            if len(images) > 0:
+                img = images[0]
+                print(img)
+                xref = img[0]
+                pix = fitz.Pixmap(doc, xref)
+                pix = fitz.Pixmap(pix, 0)
+                pix.writeImage('{}/{}.jpg'.format(str(thumbnails_dir), key))
+                break
 
 
 def read_thumbnail(key):
