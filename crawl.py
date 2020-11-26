edc971446bbaba427d318ad324e269387a124acb
tykimseoul <tykimseoul@gmail.com>
2020-11-26 17:41:22 +0900

    update: use kaggle dataset, crawl only pdfs and process later

--- crawl.py
+++ crawl.py
@@ -1,34 +1,22 @@
 import requests
-from bs4 import BeautifulSoup
 import re
 import fitz
+from bs4 import BeautifulSoup
 from pathlib import Path
-from PIL import Image
-from pdfminer.layout import LAParams, LTTextBox, LTTextLine
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.converter import PDFPageAggregator
-import jellyfish
 import time
-from random import randint
 import pandas as pd
 import json
-import schedule
+from multiprocessing.dummy import Pool as ThreadPool
+import os
 
-import numpy as np
-
-
-def get_html(url, t):
-    # print(url)
-    return requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, timeout=10)
-
-
-arxiv_base_link = 'https://arxiv.org/{}'
 temp_dir = Path("./tmp")
 temp_dir.mkdir(parents=True, exist_ok=True)
-covers_dir = Path("./tmp/covers_nn")
+covers_dir = Path("./tmp/temp")
 covers_dir.mkdir(parents=True, exist_ok=True)
+pdf_dir = Path("./tmp/pdf")
+pdf_dir.mkdir(parents=True, exist_ok=True)
+skip_dir = Path("./tmp/skip")
+skip_dir.mkdir(parents=True, exist_ok=True)
 masks_dir = Path("./tmp/masks")
 masks_dir.mkdir(parents=True, exist_ok=True)
 redaction_dir = Path("./tmp/redaction_nn")
@@ -37,199 +25,96 @@ redaction_label_dir = Path("./tmp/redaction_label_nn")
 redaction_label_dir.mkdir(parents=True, exist_ok=True)
 
 
-class TextArea:
-    def __init__(self, text=None, bbox=None):
-        self.text = text
-        self.bbox = bbox
-
-    def __str__(self):
-        return 'TextArea({}, {})'.format(self.text, self.bbox)
-
-    def __repr__(self):
-        return self.__str__()
+def get_metadata():
+    with open('./arxiv-metadata.json', 'r') as f:
+        for line in f:
+            yield line
 
 
-def crawl(start_link, count=0, t=int(time.time() * 1000)):
-    print(count)
-    if count == target_count:
-        print('file count limit reached')
-        raise Exception
-    html = get_html(start_link, 5)
-    document = BeautifulSoup(html.text, "html.parser")
+def form_dataframe():
+    metadata = get_metadata()
+    papers = []
+    for paper in metadata:
+        paper_dict = json.loads(paper)
+        category = paper_dict.get('categories').split()
+        try:
+            year = int(paper_dict.get('id')[:4])
+        except ValueError:
+            continue
+        if all(list(map(lambda c: re.match(r'cs\..+', c), category))) \
+                and 1701 <= year:
+            print(year, category)
+            paper_dict['id'] = re.sub(r'\.', '_', str(paper_dict['id']))
+            papers.append(paper_dict)
+    print(len(papers))
+    df = pd.DataFrame(papers)
+    df.drop(['versions', 'submitter', 'comments', 'license', 'doi', 'update_date', 'authors_parsed', 'report-no', 'journal-ref'], axis=1, inplace=True)
+    print(df.columns)
+    print(df.head())
+    df.to_csv('./metadata.csv')
+
+
+def crawl(id):
+    if Path('{}/{}.pdf'.format(str(pdf_dir), id)).exists() or Path('{}/{}.txt'.format(str(skip_dir), id)).exists():
+        return
+    time.sleep(1)
+    print('trying', id)
+    id = re.sub('_', '.', id)
+
     try:
-        title = document.select_one('h1.title').find(text=True, recursive=False).strip()
-        authors = document.select('div.authors > a')
-        authors = list(map(lambda a: a.text, authors))
-        abstract = document.select_one('blockquote.abstract').findAll(text=True, recursive=False)
-        abstract = ''.join(abstract).strip()
-        paper_id = document.select_one('div.extra-services > div.full-text > ul > li > a.download-pdf').get('href')
-        paper_id = re.sub(r'^/pdf/', '', paper_id)
-        key = re.sub(r'\.', '_', paper_id)
-        if Path('{}/{}.png'.format(str(covers_dir), key)).exists():
-            print('duplicate file: {}'.format(key))
-            raise Exception
-        response = requests.get('https://arxiv.org/pdf/{}'.format(paper_id))
-        save_pdf(response, paper_id, title, authors, abstract, t)
-        count += 1
-    except AttributeError:
-        pass
-    try:
-        next_link = document.select_one('span.arrow > a.next-url').get('href')
-        next_link = get_html('https://arxiv.org/{}'.format(next_link), 5).url
-        time.sleep(randint(10, 15))
-        crawl(next_link, count, t)
-    except AttributeError:
-        print(document)
-        raise Exception
+        response = requests.get('https://export.arxiv.org/pdf/{}'.format(id), timeout=(2, 3))
+    except requests.exceptions.Timeout:
+        print('timeout error')
+        return
 
-
-def save_row(file_name, pix, t, masks, title, authors, abstract):
-    try:
-        df = pd.read_csv('{}/metadata_{}.csv'.format(redaction_label_dir, t), index_col=0)
-    except FileNotFoundError:
-        df = pd.DataFrame(columns=['file', 'title', 'authors', 'abstract', 'width', 'height', 'title_bbox', 'authors_bbox', 'abstract_bbox', 'redaction_bbox'])
-    data = ['{}.png'.format(file_name), title, authors, abstract, pix.width, pix.height, json.dumps(masks[0]), json.dumps(masks[1]), json.dumps(masks[2]), json.dumps(masks[3])]
-    df = pd.concat([df, pd.DataFrame([data], columns=df.columns)])
-    df.reset_index(drop=True, inplace=True)
-    df.to_csv('{}/metadata_{}.csv'.format(redaction_label_dir, t))
-
-
-def save_pdf(response, key, title, authors, abstract, t):
-    with open('{}/paper.pdf'.format(str(temp_dir)), 'wb') as f:
+    # document = BeautifulSoup(response.text, "html.parser")
+    # try:
+    #     if document.select_one('div#content > h1').text.startswith('PDF unavailable for'):
+    #         f = open('{}/{}.txt'.format(skip_dir, re.sub('\.', '_', id)), "x")
+    #         f.close()
+    #         print('skipping', id)
+    #         return
+    # except AttributeError:
+    #     pass
+    file_name = re.sub(r'\.', '_', str(id))
+    with open('{}/paper_{}.pdf'.format(str(temp_dir), file_name), 'wb') as f:
         f.write(response.content)
-        doc = fitz.open('{}/paper.pdf'.format(str(temp_dir)))
-        pix = doc[0].getPixmap(alpha=False)
-        masks = mask_pdf(title, authors, abstract, pix)
-        if masks is None:
+        try:
+            doc = fitz.open('{}/paper_{}.pdf'.format(str(temp_dir), file_name))
+            doc.select([0])
+            doc.save('{}/{}.pdf'.format(str(pdf_dir), file_name))
+            os.remove('{}/paper_{}.pdf'.format(str(temp_dir), file_name))
+            print('saving', id)
+        except:
+            print('runtime error', file_name)
+            f = open('{}/{}.txt'.format(skip_dir, re.sub(r'\.', '_', id)), "x")
+            f.close()
+            print('skipping', file_name)
             return
-        file_name = re.sub(r'\.', '_', key)
-        save_thumbnail(file_name, pix)
-        # save_redaction(file_name, pix, masks)
-        save_row(file_name, pix, t, masks, title, authors, abstract)
-
-
-def save_thumbnail(key, pix):
-    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples).convert('L')
-    img.save('{}/{}.png'.format(str(covers_dir), key), 'PNG')
-
-
-def save_mask(key, pix, paper_size, masks):
-    mask = np.zeros((pix.height, pix.width, 3), np.uint8)
-    mask[pix.height - int(masks[0][3]):pix.height - int(masks[0][1]), int(masks[0][0]):int(masks[0][2])] = [255, 0, 0]
-    for author_mask in masks[1]:
-        mask[pix.height - int(author_mask[3]):pix.height - int(author_mask[1]), int(author_mask[0]):int(author_mask[2])] = [0, 255, 0]
-    mask[pix.height - int(masks[2][3]):pix.height - int(masks[2][1]), int(masks[2][0]):int(masks[2][2])] = [0, 0, 255]
-    mask = Image.fromarray(mask)
-    mask.save('{}/{}/{}.png'.format(str(masks_dir), paper_size, key), 'PNG')
-
-
-def save_redaction(key, pix, masks):
-    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples).convert('L')
-    img = np.array(img)
-    for bbox in masks[3]:
-        img[pix.height - int(bbox[3]):pix.height - int(bbox[1]), int(bbox[0]):int(bbox[2])] = 0
-    mask = Image.fromarray(img)
-    mask.save('{}/{}.png'.format(str(redaction_dir), key), 'PNG')
-
-
-def mask_pdf(title, authors, abstract, pix):
-    raw_boxes, text_boxes, text_lines = parse_layout()
-    if len(raw_boxes) * len(text_boxes) * len(text_lines) == 0:
-        return None
-    title_boxes = list(filter(lambda b: b.bbox[1] > pix.height / 2, raw_boxes))
-    if len(title_boxes) == 0:
-        return None
-    title_box = sorted(title_boxes, key=lambda b: measure_distance(b.text[:min(len(b.text), len(title))], title))[0]
-    author_boxes = list(filter(lambda b: b is not title_box, text_lines))
-    author_boxes = list(filter(lambda b: b.bbox[1] > pix.height / 2, author_boxes))
-    authors_boxes = list(map(lambda a: sorted(author_boxes, key=lambda b: measure_distance(b.text, a))[0], authors))
-    abstract_box = sorted(text_boxes, key=lambda b: measure_distance_for_abstracts(b.text, abstract))[0]
-    # print(normalize_text(title_box.text), list(map(lambda a: normalize_text(a.text), authors_boxes)), normalize_text(abstract_box.text))
-    return title_box.bbox, list(map(lambda b: b.bbox, authors_boxes)), abstract_box.bbox, list(map(lambda b: b.bbox, raw_boxes))
-
-
-def measure_distance(text1, text2):
-    return jellyfish.levenshtein_distance(normalize_text(text1), normalize_text(text2))
-
-
-def measure_distance_for_abstracts(text1, text2):
-    text1 = re.sub("Abstract[:-]?", "", text1, count=1)
-    text1 = re.sub("ABSTRACT[:-]?", "", text1, count=1)
-    text1 = text1[:min(300, len(text1))]
-    text2 = text2[:min(300, len(text2))]
-    return measure_distance(text1, text2)
-
-
-def normalize_text(text):
-    text = text.strip()
-    text = text.lower()
-    text = re.sub(r'\s\d+\s', '', text)
-    text = re.sub(r'\s?-\s?', '', text)
-    text = re.sub(u'\xa0', ' ', text)
-    text = re.sub(r'\n', ' ', text)
-    text = re.sub(r'\s+', ' ', text)
-    return text
-
-
-def parse_layout():
-    fp = open('{}/paper.pdf'.format(str(temp_dir)), 'rb')
-    manager = PDFResourceManager()
-    params = LAParams()
-    device = PDFPageAggregator(manager, laparams=params)
-    interpreter = PDFPageInterpreter(manager, device)
-    pages = PDFPage.get_pages(fp)
-
-    for idx, page in enumerate(pages):
-        if idx > 0:
-            break
-        interpreter.process_page(page)
-        layout = device.get_result()
-        layout = list(filter(lambda l: isinstance(l, LTTextBox), layout))
-        raw_boxes = list(map(lambda l: TextArea(l.get_text(), l.bbox), layout))
-        text_boxes = []
-        text_lines = []
-        temp_bbox = None
-        temp_text = None
-        for l in layout:
-            lines = list(filter(lambda b: isinstance(b, LTTextLine), l))
-            for line in lines:
-                tokens = re.split(r',|\d|‡|†|\*', line.get_text())
-                tokens = list(map(lambda t: TextArea(re.sub(r'^\s*and', '', t).strip(), line.bbox), tokens))
-                text_lines.extend(tokens)
-            if temp_bbox is None and temp_text is None:
-                temp_bbox = list(l.bbox)
-                temp_text = l.get_text()
-            elif l.bbox[0] != temp_bbox[0]:
-                text_box = TextArea()
-                text_box.bbox = temp_bbox
-                text_box.text = temp_text
-                text_boxes.append(text_box)
-                temp_bbox = list(l.bbox)
-                temp_text = l.get_text()
-            else:
-                temp_bbox[0] = l.bbox[0]
-                temp_bbox[1] = min(temp_bbox[1], l.bbox[1])
-                temp_bbox[2] = max(temp_bbox[2], l.bbox[2])
-                temp_bbox[3] = max(temp_bbox[3], l.bbox[3])
-                temp_text = temp_text + l.get_text()
-
-        return raw_boxes, text_boxes, text_lines
 
 
 if __name__ == '__main__':
-    target_count = 800
+    # form_dataframe()
+    # df = pd.read_csv('./metadata.csv')
+    # df = df[df['id'].str.match(r'\d{4}_\d{4,5}')]
+    # print(len(df))
+    # df = df.sample(n=30000)
+    # df.to_csv('./sampled_metadata.csv')
+    df = pd.read_csv('./sampled_metadata.csv')
+
+    processes = 8
+    chunk_size = int(len(df) / processes)
+    chunks = [df.iloc[i:i + chunk_size] for i in range(0, len(df), chunk_size)]
 
 
-    def start_crawling():
-        print('starting crawling..')
-        try:
-            crawl('https://arxiv.org/abs/{}{}.{}?context=cs'.format(str(randint(15, 19)), str(randint(1, 12)).zfill(2), str(randint(0, 100) * 100).zfill(5)))
-        except Exception:
-            print('exception caught')
-            return
+    def apply_to_df(df_chunks):
+        df_chunks = df_chunks.apply(lambda r: crawl(r['id']), axis=1)
+        return df_chunks
 
 
-    schedule.every(2).minutes.do(start_crawling)
+    # Process dataframes
+    with ThreadPool(processes) as p:
+        result = p.map(apply_to_df, chunks)
 
-    while True:
-        schedule.run_pending()
+    # Concat all chunks
+    df_reconstructed = pd.concat(result)
--- form_dataset.py
+++ form_dataset.py
@@ -6,9 +6,37 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import re
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+import jellyfish
+import fitz
+from multiprocessing import cpu_count
+from multiprocessing.dummy import Pool as ThreadPool
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+pd.set_option('max_colwidth', 100)
 
 
-def concat_dataframe():
+class TextArea:
+    def __init__(self, text=None, bbox=None):
+        self.text = text
+        self.bbox = bbox
+
+    def __str__(self):
+        return 'TextArea({}, {})'.format(self.text, self.bbox)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def dejson_dataframe(df):
+    print('dejsoning')
+
     def unwrap_columns(df, key):
         parsed = df[key].map(str).apply(json.loads).apply(pd.Series)
         df[list(map(lambda c: '{}_{}'.format(key, c), [0, 1, 2, 3]))] = parsed
@@ -19,71 +47,59 @@ def concat_dataframe():
         df['{}_3'.format(key)] = df['{}_3'.format(key)] / df['height']
         return df
 
-    dfs = []
-    for df in os.listdir('./train_data/redaction_labels_nn/'):
-        df = pd.read_csv('./train_data/redaction_labels_nn/' + df, index_col=0)
-        dfs.append(df)
-    full_df = pd.concat(dfs)
-    full_df.drop_duplicates(subset=['file'], keep='last', inplace=True)
-    redaction = full_df['redaction'].map(str).apply(json.loads).tolist()
+    df.drop_duplicates(subset=['file'], keep='last', inplace=True)
+    redaction = df['redaction_bbox'].map(str).apply(json.loads).tolist()
     redaction = list(map(lambda r: list(map(lambda a: (a[3] - a[1]) * (a[2] - a[0]), r)), redaction))
     redaction = list(map(sum, redaction))
-    full_df['redaction_area'] = redaction
-    full_df = unwrap_columns(full_df, 'title')
-    full_df = unwrap_columns(full_df, 'abstract')
-    full_df['redaction_area'].hist(bins=1000)
-    print(len(full_df))
-    plt.show()
-    full_df.to_csv('./train_data/full_df.csv')
-    print(full_df.head(10))
-
-
-def filter_dataframe():
-    df = pd.read_csv('./train_data/full_df.csv', index_col=0)
-    print(len(df))
-    df.drop(df[df['abstract_area'] < 4000].index, inplace=True)
-    df.drop(df[df['redaction_area'] < 40000].index, inplace=True)
-    df.drop(df[df['redaction_area'] > 350000].index, inplace=True)
-    # plt.figure()
-    # df['abstract_area'].hist(bins=100)
+    df['redaction_area'] = redaction
+    df = unwrap_columns(df, 'title_bbox')
+    df = unwrap_columns(df, 'abstract_bbox')
+    # df['redaction_area'].hist(bins=1000)
     # plt.show()
-    plt.figure()
-    df['redaction_area'].hist(bins=1000)
-    plt.show()
-    plt.figure()
-    plt.scatter(df['redaction_area'], df['title_area'])
-    plt.axes().set_aspect('equal', adjustable='box')
-    plt.show()
-    plt.figure()
-    plt.scatter(df['redaction_area'], df['abstract_area'])
-    plt.axes().set_aspect('equal', adjustable='box')
-    plt.show()
-    df.drop(['redaction', 'title', 'abstract'], axis=1, inplace=True)
-    print(len(df))
+    print(df.head(10))
     return df
 
 
-concat_dataframe()
+def filter_dataframe(df):
+    print('filtering')
+    df.drop(['redaction_bbox', 'title_bbox', 'authors_bbox', 'abstract_bbox'], axis=1, inplace=True)
+    df.drop(df[df['width'] != 612].index, inplace=True)
+    df.drop(df[df['height'] != 792].index, inplace=True)
+    df.drop(df[df['abstract_bbox_area'] > 250000].index, inplace=True)
+    df.drop(df[df['abstract_bbox_area'] < 10000].index, inplace=True)
+    df.drop(df[df['title_bbox_area'] > 50000].index, inplace=True)
+    df.drop(df[df['title_bbox_area'] < 1000].index, inplace=True)
+    df.drop(df[df['redaction_area'] < 40000].index, inplace=True)
+    df.drop(df[df['redaction_area'] > 350000].index, inplace=True)
+    df.drop(df[df['abstract_bbox_3'] > df['title_bbox_1']].index, inplace=True)
+    # plt.figure()
+    # df['title_bbox_area'].hist(bins=100)
+    # plt.show()
+    # plt.figure()
+    # df['abstract_bbox_area'].hist(bins=100)
+    # plt.show()
+    df.reset_index(drop=True, inplace=True)
+    return df
 
-train_dir = Path("./train_data/to_train")
-train_dir.mkdir(parents=True, exist_ok=True)
-shutil.rmtree('./train_data/to_train')
+
+train_dir = Path("./train_data/train_unet")
+shutil.rmtree(train_dir)
 train_dir.mkdir(parents=True, exist_ok=True)
 area_dir = Path('./train_data/area')
 area_dir.mkdir(parents=True, exist_ok=True)
-mask_dir = Path('./train_data/mask')
+mask_dir = Path('./train_data/masks_unet')
+shutil.rmtree(mask_dir)
 mask_dir.mkdir(parents=True, exist_ok=True)
+covers_dir = Path('./tmp/thumbnail')
+shutil.rmtree(covers_dir)
+covers_dir.mkdir(parents=True, exist_ok=True)
+pdf_dir = Path("./tmp/pdf")
+pdf_dir.mkdir(parents=True, exist_ok=True)
 
 
-def save_as_image(key, abstract_0, abstract_1, abstract_2, abstract_3):
-    img = np.round(np.array([abstract_0, abstract_1, abstract_2, abstract_3]) * 255, decimals=0)
-    img = Image.fromarray(img).convert('L')
-    img.save('{}/{}'.format(area_dir, key))
-
-
-def move_file(file):
+def copy_thumbnail(file):
     try:
-        shutil.copy('./train_data/redaction_nn/' + file, train_dir)
+        shutil.copy('{}/{}.png'.format(str(covers_dir), file), train_dir)
     except FileNotFoundError:
         pass
 
@@ -93,11 +109,156 @@ def make_mask(key, width, height, title_0, title_1, title_2, title_3, abstract_0
     img[height - int(title_3 * height): height - int(title_1 * height), int(title_0 * width):int(title_2 * width), :] = [255, 0, 0]
     img[height - int(abstract_3 * height): height - int(abstract_1 * height), int(abstract_0 * width):int(abstract_2 * width), :] = [0, 0, 255]
     img = Image.fromarray(img)
-    img.save('{}/{}'.format(mask_dir, key))
+    img.save('{}/{}.png'.format(mask_dir, key))
 
 
-df = filter_dataframe()
-df.to_csv('./train_data/filtered.csv')
-df.apply(lambda r: save_as_image(r['file'], r['abstract_0'], r['abstract_1'], r['abstract_2'], r['abstract_3']), axis=1)
-df.apply(lambda r: move_file(r['file']), axis=1)
-df.apply(lambda r: make_mask(r['file'], r['width'], r['height'], r['title_0'], r['title_1'], r['title_2'], r['title_3'], r['abstract_0'], r['abstract_1'], r['abstract_2'], r['abstract_3']), axis=1)
+def parse(samples_df, pdfs):
+    rows = []
+    for pdf in pdfs:
+        print(pdf)
+        try:
+            doc = fitz.open('{}/{}'.format(str(pdf_dir), pdf), filetype="pdf")
+            pix = doc[0].getPixmap(alpha=False)
+            id = re.sub(r'\.pdf$', '', pdf)
+            save_thumbnail(id, pix)
+            fp = open('{}/{}'.format(str(pdf_dir), pdf), 'rb')
+            row = samples_df[samples_df['id'] == id].iloc[0]
+            masks = mask_pdf(fp, row['title'], re.split(r',\s+', row['authors']), row['abstract'], pix)
+            if masks is None:
+                continue
+            parsed_row = parse_row(row['id'], pix, masks)
+            rows.append(parsed_row)
+        except RuntimeError:
+            continue
+    df = pd.DataFrame(rows)
+    if len(df) == 0:
+        return pd.DataFrame(columns=['file', 'width', 'height', 'title_bbox', 'authors_bbox', 'abstract_bbox', 'redaction_bbox'])
+    df.columns = ['file', 'width', 'height', 'title_bbox', 'authors_bbox', 'abstract_bbox', 'redaction_bbox']
+    return df
+
+
+def save_thumbnail(key, pix):
+    img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples).convert('L')
+    img.save('{}/{}.png'.format(covers_dir, key), 'PNG')
+
+
+def parse_row(file_name, pix, masks):
+    return [file_name, pix.width, pix.height, json.dumps(masks[0]), json.dumps(masks[1]), json.dumps(masks[2]), json.dumps(masks[3])]
+
+
+def mask_pdf(fp, title, authors, abstract, pix):
+    raw_boxes, text_boxes, text_lines = parse_layout(fp)
+    if len(raw_boxes) * len(text_boxes) * len(text_lines) == 0:
+        return None
+    title_boxes = list(filter(lambda b: b.bbox[1] > pix.height / 2, raw_boxes))
+    if len(title_boxes) == 0:
+        return None
+    title_box = sorted(title_boxes, key=lambda b: measure_distance(b.text[:min(len(b.text), len(title))], title))[0]
+    author_boxes = list(filter(lambda b: b is not title_box, text_lines))
+    author_boxes = list(filter(lambda b: b.bbox[1] > pix.height / 2, author_boxes))
+    authors_boxes = list(map(lambda a: sorted(author_boxes, key=lambda b: measure_distance(b.text, a))[0], authors))
+    abstract_box = sorted(text_boxes, key=lambda b: measure_distance_for_abstracts(b.text, abstract))[0]
+    # print(normalize_text(title_box.text), list(map(lambda a: normalize_text(a.text), authors_boxes)), normalize_text(abstract_box.text))
+    return title_box.bbox, list(map(lambda b: b.bbox, authors_boxes)), abstract_box.bbox, list(map(lambda b: b.bbox, raw_boxes))
+
+
+def measure_distance(text1, text2):
+    return jellyfish.levenshtein_distance(normalize_text(text1), normalize_text(text2))
+
+
+def measure_distance_for_abstracts(text1, text2):
+    text1 = re.sub("Abstract[:-]?", "", text1, count=1)
+    text1 = re.sub("ABSTRACT[:-]?", "", text1, count=1)
+    text1 = text1[:min(300, len(text1))]
+    text2 = text2[:min(300, len(text2))]
+    return measure_distance(text1, text2)
+
+
+def normalize_text(text):
+    text = text.strip()
+    text = text.lower()
+    text = re.sub(r'\s\d+\s', '', text)
+    text = re.sub(r'\s?-\s?', '', text)
+    text = re.sub(u'\xa0', ' ', text)
+    text = re.sub(r'\n', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    return text
+
+
+def parse_layout(fp):
+    manager = PDFResourceManager()
+    params = LAParams()
+    device = PDFPageAggregator(manager, laparams=params)
+    interpreter = PDFPageInterpreter(manager, device)
+    pages = PDFPage.get_pages(fp)
+
+    for idx, page in enumerate(pages):
+        if idx > 0:
+            break
+        interpreter.process_page(page)
+        layout = device.get_result()
+        layout = list(filter(lambda l: isinstance(l, LTTextBox), layout))
+        raw_boxes = list(map(lambda l: TextArea(l.get_text(), l.bbox), layout))
+        text_boxes = []
+        text_lines = []
+        temp_bbox = None
+        temp_text = None
+        for l in layout:
+            lines = list(filter(lambda b: isinstance(b, LTTextLine), l))
+            for line in lines:
+                tokens = re.split(r',|\d|‡|†|\*', line.get_text())
+                tokens = list(map(lambda t: TextArea(re.sub(r'^\s*and', '', t).strip(), line.bbox), tokens))
+                text_lines.extend(tokens)
+            if temp_bbox is None and temp_text is None:
+                temp_bbox = list(l.bbox)
+                temp_text = l.get_text()
+            elif l.bbox[0] != temp_bbox[0]:
+                text_box = TextArea()
+                text_box.bbox = temp_bbox
+                text_box.text = temp_text
+                text_boxes.append(text_box)
+                temp_bbox = list(l.bbox)
+                temp_text = l.get_text()
+            else:
+                temp_bbox[0] = l.bbox[0]
+                temp_bbox[1] = min(temp_bbox[1], l.bbox[1])
+                temp_bbox[2] = max(temp_bbox[2], l.bbox[2])
+                temp_bbox[3] = max(temp_bbox[3], l.bbox[3])
+                temp_text = temp_text + l.get_text()
+
+        return raw_boxes, text_boxes, text_lines
+
+
+def form_dataset(pdfs):
+    samples_df = pd.read_csv('./sampled_metadata.csv')
+    df = parse(samples_df, pdfs)
+
+    df = dejson_dataframe(df)
+    df = filter_dataframe(df)
+    df.apply(lambda r: copy_thumbnail(r['file']), axis=1)
+    df.apply(lambda r: make_mask(r['file'], r['width'], r['height'], r['title_bbox_0'], r['title_bbox_1'], r['title_bbox_2'], r['title_bbox_3'], r['abstract_bbox_0'], r['abstract_bbox_1'], r['abstract_bbox_2'], r['abstract_bbox_3']), axis=1)
+    return df
+
+
+processes = cpu_count()
+pdfs = sorted(os.listdir(pdf_dir))[:20]
+pdf_chunk_size = int(len(pdfs) / processes)
+pdf_chunks = [pdfs[i:i + pdf_chunk_size] for i in range(0, len(pdfs), pdf_chunk_size)]
+print(list(map(len, pdf_chunks)))
+assert sum(list(map(len, pdf_chunks))) == len(pdfs)
+
+
+def apply_to_df(pdf_chunk):
+    pdf_chunk = form_dataset(pdf_chunk)
+    return pdf_chunk
+
+
+# Process dataframes
+with ThreadPool(processes) as p:
+    result = p.map(apply_to_df, pdf_chunks)
+
+# Concat all chunks
+df_reconstructed = pd.concat(result)
+df_reconstructed.reset_index(drop=True, inplace=True)
+print('done', len(df_reconstructed))
+df_reconstructed.to_csv('./train_data/filtered_metadata.csv')
