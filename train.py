3ed29e6f3489768dff7bf7ab1a0c2e346bd8a725
Taeyoon Kim <tykimseoul@gmail.com>
2020-12-05 15:35:54 +0900

    update: replace dropout with batch normalization

--- train.py
+++ train.py
@@ -5,6 +5,36 @@ from tensorflow.keras.callbacks import *
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from scipy import ndimage
+import tensorflow.keras.backend as K
+
+
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return 1 - dice_coef(y_true, y_pred)
+
+
+def tversky(y_true, y_pred, smooth=1, alpha=0.7):
+    y_true_pos = K.flatten(y_true)
+    y_pred_pos = K.flatten(y_pred)
+    true_pos = K.sum(y_true_pos * y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
+    false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
+    return (true_pos + smooth) / (true_pos + alpha * false_neg + (1 - alpha) * false_pos + smooth)
+
+
+def tversky_loss(y_true, y_pred):
+    return 1 - tversky(y_true, y_pred)
+
+
+def focal_tversky_loss(y_true, y_pred, gamma=0.75):
+    tv = tversky(y_true, y_pred)
+    return K.pow((1 - tv), gamma)
 
 
 class Unet:
@@ -18,51 +48,68 @@ class Unet:
         inputs = Input(self.input_size)
 
         conv1 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+        conv1 = BatchNormalization()(conv1)
         conv1 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+        conv1 = BatchNormalization()(conv1)
         pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
         conv2 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        conv2 = BatchNormalization()(conv2)
         conv2 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+        conv2 = BatchNormalization()(conv2)
         pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
         conv3 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        conv3 = BatchNormalization()(conv3)
         conv3 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+        conv3 = BatchNormalization()(conv3)
         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
         conv4 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = BatchNormalization()(conv4)
         conv4 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
-        drop4 = Dropout(0.5)(conv4)
-        pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+        conv4 = BatchNormalization()(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
         conv5 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+        conv5 = BatchNormalization()(conv5)
         conv5 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-        drop5 = Dropout(0.5)(conv5)
+        conv5 = BatchNormalization()(conv5)
 
-        up6 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
-        merge6 = concatenate([drop4, up6], axis=3)
+        up6 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv5))
+        merge6 = concatenate([conv4, up6], axis=3)
         conv6 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+        conv6 = BatchNormalization()(conv6)
         conv6 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+        conv6 = BatchNormalization()(conv6)
 
         up7 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
         merge7 = concatenate([conv3, up7], axis=3)
         conv7 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+        conv7 = BatchNormalization()(conv7)
         conv7 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+        conv7 = BatchNormalization()(conv7)
 
         up8 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
         merge8 = concatenate([conv2, up8], axis=3)
         conv8 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+        conv8 = BatchNormalization()(conv8)
         conv8 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+        conv8 = BatchNormalization()(conv8)
 
         up9 = Conv2D(32, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
         merge9 = concatenate([conv1, up9], axis=3)
         conv9 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+        conv9 = BatchNormalization()(conv9)
         conv9 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = BatchNormalization()(conv9)
         conv9 = Conv2D(self.num_class, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = BatchNormalization()(conv9)
         conv10 = Conv2D(self.num_class, 1, activation='softmax')(conv9)
 
         model = Model(inputs, conv10)
 
-        model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics='accuracy')
+        model.compile(optimizer=Adam(lr=1e-4), loss=focal_tversky_loss, metrics=[tversky,  'accuracy'])
 
         model.summary()
 
@@ -71,9 +118,12 @@ class Unet:
 
 def adjust_data(img, mask, num_class):
     def delineate(img):
+        def sigmoid(x, c):
+            return 1 / (1 + np.exp(-c * x))
+
         def sobel(img, size):
             img = ndimage.maximum_filter(img, size=size)
-            img = np.around(img)
+            img = sigmoid(img, 5)
             sx = ndimage.sobel(img, axis=0, mode='constant')
             sy = ndimage.sobel(img, axis=1, mode='constant')
             sobel = np.hypot(sx, sy)
